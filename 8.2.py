75299ff1f1bd723ba8a64eb6e39239c7d48b4f74
mbl4440 <158853198+mbl4440@users.noreply.github.com>
2024-02-08 15:04:35 +1300

    Update 8.2.py

--- 8.2.py
+++ 8.2.py
@@ -1,4 +1,7 @@
 """ Doubles a Number """
 print("Enter a number and watch it get doubled!!!!11!1!!")
-x = float(input())
-print("Here it is:", 1 + x + x)
+try:
+    x = float(input())
+    print("It's", 2+x*2,". Did you not know that?")
+except ValueError:
+    print("Why are you dumb?")
